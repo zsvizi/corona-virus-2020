2be8894b55365a022ee459e7f670b955ee438dd8
Zsolt Vizi <vizizsolt89@gmail.com>
2020-02-06 17:55:40 +0100

    Add finalized risk.py and heatmap.py

--- source/heatmap.py
+++ source/heatmap.py
@@ -4,124 +4,129 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors as c
 import matplotlib.ticker as ticker
+from decimal import Decimal
 
-from source.risk import get_heatmap
+from risk import get_heatmap
 
-# DIMENSIONS: t_star x connectivity (theta) x r_loc (~z)
-param = 0
-coords = [1, 2]
-vals = [1.05]
-filename = "../data"
+'''
+Hello
+heatmap.py plots the output of risk.py as a heatmap graph with the given parameters.
+
+The risk is a function of 3 variables: C, theta and r_loc. We follow this order in this script.
+        - i) fix one variable: param = (YOUR SELECTED VARIABLE) 
+                    ex. param = 0, that is "C"  (as Python idexes from 0) 
+                    or param = 1, that is "theta"
+                    or param = 2, that is "r_loc"
+        - ii) say, you selected param = 2 (r_loc) to be fixed, now you can plot as many heatmaps as many       r_loc values you provide to the program in r_loc = [(YOUR LIST OF FIXED PARAMETERS)]
+                    ex. r_loc = np.array([1.3,1.8,2.2,2.6])
+        - iii) specify the range of the other two variables you want to examine
+                    ex. c = np.arange(100000, 1100000, 10000) 
+                    and theta = np.arange(0, 0.000255, 0.000001)
+        - iv) The program saves the heatmaps in a folder named after your selected variable in the             "Heatmaps" folder
+'''
+#----------------------------------------------------        
 
 
-def get_data(path):
-    max_number_summands = 100
-    read = False
-    if read:
-        coord_list = read_data(path)
-        r_loc = coord_list[2]
-        theta = coord_list[1]
-        t_star = coord_list[0]
-        del coord_list[:3]
-    else:
-        heatmap, _ = get_heatmap(max_number_summands)
-        t_star = heatmap["t_stars"]
-        theta = heatmap["theta"]
-        r_loc = heatmap["r_locs"]
-        coord_list = heatmap["heatmap"]
+# Give the values here:
+param = 2           #param = 0 ( = C); param = 1 ( = theta); param = 2 ( = r_loc)
+
+c = np.arange(100000, 1100000, 10000)
+theta = np.arange(0, 0.000255, 0.000001)
+r_loc = np.array([1.05, 1.3, 1.8, 2.2, 2.6])
+#r_loc = np.arange(1, 2.5, 0.1)
+
+#----------------------------------------------------        
+
+# Don't touch this :p
+
+coords = [0,1,2]
+coords.pop(param)
+vals = [c, theta, r_loc][param]
+name_of_coords = ["C", "$\\theta$", "$R_{loc}$"]
+
+def get_data(c, theta, r_loc):
+    heatmap, _ = get_heatmap(c, theta, r_loc)
+    c = heatmap["r_stars"]
+    theta = heatmap["theta"]
+    r_loc = heatmap["r_locs"]
+    coord_list = heatmap["heatmap"]
     
-    return [[t_star, theta, r_loc], coord_list]
+    return [[c, theta, r_loc], coord_list]
 
 
-def read_data(path):
-    # beolvassa az aktuális (nr.txt) fájlt, soronként feldarabolja és a ccord_list-be teszi
-    with open(path, "r") as coord_file:
-        coord_list = coord_file.read().splitlines()
-    # végigmegy a coord_list elemein és a "," mentén feldarabolja
-    for number in range(len(coord_list)):
-        row = coord_list[number].split(",")
-        coord_list[number] = []
-        for elem in row:
-            coord_list[number].append(float(elem))
-    return coord_list
-
-
-def preprocess(fname):
-    vars, raw_data = get_data(fname)
+def preprocess():
+    variables, raw_data = get_data(c, theta, r_loc)
     data = []
+    
     for value in vals:
         temp_data = []
-        for element in raw_data:    # végig megy a teljes nyers adatsoron
-            if abs(element[param] - value) < 10 ** (-4):  # kiválogatja a megadott paraméter szerint a kért értékeket
-                # hozzáadja egy listához
+        for element in raw_data:    # goes through raw_data
+            # selects required values
+            if abs(element[param] - value) < 10 ** (-15):  
+                # ads it to a list
                 temp_data.append([element[coords[0]], element[coords[1]], element[-1]])
         data.append(temp_data)
-    data = np.array(data)
-    return [vars, data]
+    data = np.array(data) 
+    return [variables, data]
 
 
-def plot_heatmap(dat,i,foldr):
+def plot_heatmap(variables, vals, dat,i,foldr):
     
     X = variables[coords[0]]
     Y = variables[coords[1]]
-    """
-    for value in Y:
-        lst = []
-        for row in dat:
-            if row[1] == value:
-                lst.append(row[-1])
-        Z.append(lst)
-    """
+    
     dat = np.array(dat[:, -1]).reshape(len(X), len(Y))
     Z = dat.T
 
     fig, ax = plt.subplots()
-    contours = plt.contour(X, Y, Z, [0.3,0.4,0.5], colors='#000000')
-    plt.clabel(contours, inline=True, fontsize=8)
-    plt.contourf(X, Y, Z, 10, cmap='RdGy', alpha=0.5)
+    plt.contourf(X, Y, Z, 50, cmap='Reds', alpha=1)
 
     ax.xaxis.set_major_locator(ticker.AutoLocator())
     ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())   
-    
     ax.yaxis.set_major_locator(ticker.AutoLocator())
     ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
-    
-    if coords[0] == 0: X_name = "local reproduction number"
-    elif coords[0] == 1: X_name = "Theta"
-    elif coords[0] == 2: X_name = "R*"
-    
-    if coords[1] == 0: Y_name = "local reproduction number"
-    elif coords[1] == 1: Y_name = "Theta"
-    elif coords[1] == 2: Y_name = "R*"
 
-    plt.xlabel(X_name)
-    plt.ylabel(Y_name)
-    
+    ax.set_title(name_of_coords[param]+' = ' + str(vals[i]), fontsize=10)
+    plt.xlabel(name_of_coords[coords[0]])
+    plt.ylabel(name_of_coords[coords[1]])
+
+    plt.clim(0,1)
     plt.colorbar()
+
+    plt.tight_layout()
     plt.savefig(foldr+"/"+str(vals[i])+".png", dpi=300, facecolor='w', edgecolor='w',
                 orientation='portrait', papertype=None, format=None,
                 transparent=False, bbox_inches=None, pad_inches=0.1,
                 frameon=None)
+    
     plt.clf()
 
+#----------------------------------------------------        
+    
 
 def makedir():
-    if param == 0: foldr = "./R_loc"
-    elif param == 1: foldr = "./theta"
-    elif param == 2: foldr = "./t_star"
-    else: foldr = "valami_baly_van!4444!!"+str(vals)
-    
+    names = ["C", "Theta", "R_loc"]
+    foldr = "./Heatmaps/" + names[param]
     try:
         os.makedirs(foldr)
     except:
-        dt = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
-        foldr = foldr+"-"+dt
+        foldr = foldr+"-"+datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
         os.makedirs(foldr)
-    
     return foldr
 
-foldr = makedir()
 
-variables, data = preprocess(filename)
-for i in range(len(data)):
-    plot_heatmap(data[i],i,foldr)
+#----------------------------------------------------      
+
+
+def main():
+    foldr = makedir()       
+    variables, data = preprocess()
+
+    for i in range(len(data)):
+        plot_heatmap(variables, vals, data[i], i, foldr)
+    
+    
+
+if __name__ == "__main__":
+    main()
+
--- source/risk.py
+++ source/risk.py
@@ -1,109 +1,101 @@
-import numpy as np
-import itertools
 from scipy import optimize
 from scipy.stats import binom
-from source.main_SEIR import solve_controlled_seir
-from source.risk_test import test_function
+import math
+import random
+import numpy as np
+import itertools
+import sys
 
+'''
+risk.py calculates the risk of an epidemic in a destination country.
 
-def get_heatmap(max_number_summands):
-    # Compute z
-    r_locs = np.arange(1.0, 4.0, 0.01)
-    extinction_probability = compute_extinction_probability(r_locs)
+'''
 
-    # Get final sizes via solving SEIR models
-    r0 = 2.6
-    final_size, t_stars = get_final_size(r0)
-    connectivity_ratio = np.arange(0, 0.2, 0.01)
-
+def get_heatmap(c, theta, r_loc):
+    r_stars = c
+    connectivity_ratio = theta
+    r_locs = r_loc
+    
+    z = compute_z(r_locs)
+    
     # Compute risks for possible combinations
-    risk_ravel, order = compute_risk(final_sizes=final_size,
-                                     connectivity_ratio=connectivity_ratio,
-                                     extinction_probability=extinction_probability,
-                                     max_number=max_number_summands)
+    risk_ravel, order = compute_risk(r_stars=r_stars, connectivity_ratio=connectivity_ratio, solution=z)
 
     # Get all combinations of variables
     all_combinations = get_combinations(r_locs=r_locs,
                                         connectivity_ratio=connectivity_ratio,
-                                        t_stars=t_stars, order=order)
+                                        r_stars=r_stars, order=order)
 
-    heatmap = {"heatmap": np.append(all_combinations, risk_ravel, axis=1), "t_stars": t_stars,
+    heatmap = {"heatmap": np.append(all_combinations, risk_ravel, axis=1), "r_stars": r_stars,
                "theta": connectivity_ratio, "r_locs": r_locs}
     return heatmap, order
 
 
-def compute_risk(final_sizes, connectivity_ratio, extinction_probability, max_number=100):
-    # DIMENSIONS: t_star x connectivity (theta) x r_loc (~z)
-    risk = np.zeros((len(final_sizes), len(connectivity_ratio), len(extinction_probability)))
-    j, k = [0, 0]
-    for fs in final_sizes:
-        k = 0
-        for theta in connectivity_ratio:
-            # Calculate binomial probabilities
-            probs = binom.pmf(np.arange(max_number), fs, theta)
-            # Calculate vector of risks corresponding to a specified fs and theta
-            risk[j, k, :] = np.dot(np.array(probs), 1 - extinction_probability ** np.arange(max_number).reshape(-1, 1))
-            k += 1
-        j += 1
-    # Order of previous loops determines order of variables - change IFF loop is changed!
-    order = {"r_locs": 2, "connectivity_ratio": 1, "t_stars": 0}
+def compute_z(r_locs):
+    solution = optimize.fixed_point(generator_function_neg_binom, 0.5 * np.ones(r_locs.shape[0]), args=(r_locs,))
+    return solution
+
+
+def generator_function_neg_binom(z, r_loc):
+    # Parameters: k (dispersion), p
+    # Mean: (1-p)*k/p = r_loc -> p = k / (k + r_loc)
+    # PGF:((1-p) / (1 - p*z))**k
+    # Dispersion parameter - TO BE DETERMINED
+    k = 0.64
+    p = k / (k + r_loc)
+    return np.power(p / (1 - (1 - p) * z), k)
+
+def compute_risk(r_stars, connectivity_ratio, solution):
+    # DIMENSIONS: r_star (final size at t_star, later maybe t_star) x connectivity (theta) x r_loc (~z)
+    risk = np.zeros((len(r_stars), len(connectivity_ratio), len(solution)))
+    allin = len(r_stars)*len(connectivity_ratio)
+    k=0
+    for (r_starIndex, r_star) in enumerate(r_stars):
+        for (thetaIndex, theta) in enumerate(connectivity_ratio):
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
+            print(str(allin) + "/" + str(k))
+            risk[r_starIndex, thetaIndex, :] = 1 - ((1 - theta) + theta * solution) ** r_star
+            k+=1
+    # Order of previous loops determines order of variables
+    order = {"r_locs": 2, "connectivity_ratio": 1, "r_stars": 0}
     risk_ravel = risk.reshape((-1, 1))
     return risk_ravel, order
 
 
-def get_final_size(r0):
-    # Get t_stars, final_sizes, possible R0s
-    t_stars, final_sizes, r_0_dict = solve_controlled_seir()
-    # Get final sizes corresponding to the specified r0
-    final_size_idx = r_0_dict[r0]
-    final_size = final_sizes[final_size_idx]
-    return final_size, t_stars
-
-
-def compute_extinction_probability(r_locs):
-    extinction_probability = \
-        optimize.fixed_point(generator_function_neg_binom, 0.5 * np.ones(r_locs.shape[0]), args=(r_locs,))
-    return extinction_probability
-
-
-def generator_function_neg_binom(z, r_loc):
-    # Parameters of negative binomial distribution: k (dispersion), p
-    # Mean: (1-p)*k/p = r_loc -> p = k / (k + r_loc)
-    # PGF:((1-p) / (1 - p*z))**k
-    k = 2.2
-    p = k / (k + r_loc)
-    return np.power(p / (1 - (1 - p) * z), k)
-
-
-def get_combinations(r_locs, t_stars, connectivity_ratio, order):
-    # DIMENSIONS: t_star x connectivity (theta) x r_loc (~z)
+def get_combinations(r_locs, r_stars, connectivity_ratio, order):
+    # DIMENSIONS: r_star (final size at t_star, later maybe t_star) x connectivity (theta) x r_loc (~z)
     all_combinations_array = [[], [], []]
-    all_combinations_array[order["t_stars"]] = t_stars
+    all_combinations_array[order["r_stars"]] = r_stars
     all_combinations_array[order["connectivity_ratio"]] = connectivity_ratio
     all_combinations_array[order["r_locs"]] = r_locs
     all_combinations = np.array(list(itertools.product(*all_combinations_array)))
     return all_combinations
 
 
+def write_file(connectivity_ratio, heat_map, r_locs, r_stars):
+    with open("../data/heatmap_to_save.txt", "a+") as file:
+        file.write(str(r_locs)[1:-1])
+        file.write("\n")
+
+        file.write(str(connectivity_ratio)[1:-1])
+        file.write("\n")
+
+        file.write(str(r_stars)[1:-1])
+        file.write("\n")
+
+        for vector in heat_map:
+            file.write(str(vector)[1:-1])
+            file.write("\n")
+
+            
 def main():
-    max_number_summands = 100
     # Get heat map
-    heatmap, order = get_heatmap(max_number_summands)
+    heatmap, order = get_heatmap()
     heat_map = heatmap["heatmap"]
-    t_stars = heatmap["t_stars"]
+    r_stars = heatmap["r_stars"]
     connectivity_ratio = heatmap["theta"]
     r_locs = heatmap["r_locs"]
-    # Check shape of heatmap
-    print(heat_map.shape)
-
-    test_function(heat_map=heat_map,
-                  t_stars=t_stars,
-                  connectivity_ratio=connectivity_ratio,
-                  r_locs=r_locs,
-                  max_number_summands=max_number_summands,
-                  order=order)
-
 
 if __name__ == "__main__":
     main()
-
